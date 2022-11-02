21661bccf3be25ad3083774f8767d661ed980c16
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-11-02 23:24:45 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('yeeey I am clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
